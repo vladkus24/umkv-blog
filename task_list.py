59cff23be0b7f0967d6f62623274c05047e5917c
monKey <21fi.v.dziuba@std.npu.edu.ua>
2024-04-21 19:35:41 +0300

    classes

--- task_list.py
+++ task_list.py
@@ -3,3 +3,15 @@ from tkinter import messagebox, ttk
 from tkcalendar import DateEntry
 from datetime import datetime
 
+class Task:
+    def __init__(self, name, priority, due_date):
+        self.name = name
+        self.priority = priority
+        self.due_date = due_date
+
+class TaskManagerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Планувальник")
+
+        self.tasks = []
